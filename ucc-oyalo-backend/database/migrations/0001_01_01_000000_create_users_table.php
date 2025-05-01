65be8d4383f6a9c89717106103461d2661d0cb22
AbelTuoyintir <abeltuoyintir07@gmail.com>
2025-05-01 12:22:51 -0700

    update on user migration

--- ucc-oyalo-backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ ucc-oyalo-backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,7 @@ return new class extends Migration
             $table->string('email')->nullable()->unique(); ;
             $table->string('phone')->nullable()->unique(); ;
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            // $table->foreignId('role')
+            $table->string('clerkID');
             $table->string('address')->nullable();
             $table->string('image')->nullable();
             $table->rememberToken();
